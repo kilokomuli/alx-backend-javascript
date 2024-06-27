cc998aab37160faf9afd73a9d4eaec794339d7ef
Emma <kilokoemma175@gmail.com>
2024-06-27 18:32:57 +0000

    correction

--- 0x02-ES6_classes/1-make_classrooms.js
+++ 0x02-ES6_classes/1-make_classrooms.js
@@ -1,10 +1,5 @@
-import ClassRoom from './0-classroom.js';
+import ClassRoom from './0-classroom';
 
 export default function initializeRooms() {
-  const cls1 = new ClassRoom(19);
-  const cls2 = new ClassRoom(20);
-  const cls3 = new ClassRoom(34);
-  const classes = [cls1, cls2, cls3];
-  
-  return (classes);
+  return [19, 20, 34].map((size) => new ClassRoom(size));
 }
