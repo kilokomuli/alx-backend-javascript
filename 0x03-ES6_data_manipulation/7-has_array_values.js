e468ac784c52f304cc3191c93191c6ab40e2daca
Emma <kilokoemma175@gmail.com>
2024-07-01 20:26:44 +0000

    fix linter

--- 0x03-ES6_data_manipulation/7-has_array_values.js
+++ 0x03-ES6_data_manipulation/7-has_array_values.js
@@ -1,7 +1,7 @@
 export default function hasValuesFromArray(set, array) {
-  for (let value of array) {
+  for (const value of array) {
     if (!set.has(value)) {
-      return  false;
+      return false;
     }
   }
   return true;
