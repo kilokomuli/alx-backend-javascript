779c34fa4ab45b86a371d6b4e7ad7224da94ff65
kilokomuli <kilokoemma175@gmail.com>
2024-08-21 01:43:49 +0000

    4

---
+++ 0x05-Node_JS_basic/4-http.js
@@ -0,0 +1,15 @@
+const { createServer } = require('http');
+
+const hostname = '127.0.0.1';
+const port = 1245;
+
+const app = createServer((req, res) => {
+  response.statusCode = 200;
+  response.setHeader('Content-Type', 'text/plain');
+  response.end('Hello Holberton School!');
+});
+
+app.listen(port, hostname, () => {
+});
+
+module.exports = app;
