a1e67c6d8c8fd00d420938d5c380100a5c926044
kilokomuli <kilokoemma175@gmail.com>
2024-08-21 01:16:05 +0000

    task3

--- 0x05-Node_JS_basic/2-read_file.js
+++ 0x05-Node_JS_basic/2-read_file.js
@@ -5,7 +5,7 @@ function countStudents(fileName) {
   const fields = {};
   let length = 0;
   try {
-    const fileContents = fs.readFileSync(fileName,'utf-8');
+    const fileContents = fs.readFileSync(fileName, 'utf-8');
     const lines = fileContents.toString().split('\n');
     for (let i = 0; i < lines.length; i += 1) {
       if (lines[i]) {
