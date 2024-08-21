6deb20ad4ae4e1f46f820c5e9d4876fb59ef9d99
kilokomuli <kilokoemma175@gmail.com>
2024-08-21 00:29:12 +0000

    task0

---
+++ 0x05-Node_JS_basic/0-console.js
@@ -0,0 +1,7 @@
+/**
+ *  create a function named displayMessage
+ *  Prints in STDOUT the string argument
+ */
+const displayMessage = (message) => console.log(message);
+
+module.exports = displayMessage;
