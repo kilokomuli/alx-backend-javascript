44edf3dd28d5b7e4b9db790daf08164cf3379cab
kilokomuli <kilokoemma175@gmail.com>
2024-08-23 01:06:55 +0000

    task2

---
+++ 0x06-unittests_in_js/1-calcul.js
@@ -0,0 +1,10 @@
+const calculateNumber = (type, a, b) => {
+  const roundea = Math.round(a);
+  const roundedB = Math.round(b);
+
+  switch(type) {
+    case 'SUM':
+		  retusnr 
+};
+
+module.exports = calculateNumber;
