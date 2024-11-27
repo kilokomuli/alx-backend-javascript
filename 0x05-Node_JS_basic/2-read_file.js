139e27c345fed5cdba1424b629d1e735f934dc96
Emma <kilokoemmma175@gmail.com>
2024-11-27 15:10:29 +0000

    task2

---
+++ 0x05-Node_JS_basic/2-read_file.js
@@ -0,0 +1,27 @@
+const fs = require('fs')
+
+function countStudents(path) {
+  try {
+    const data = fs.readFileSync(path, 'utf8');
+    const lines = data.split('\n').filter((line) => line.trim() !== '' && !line.startsWith('firstname'));
+    if (lines.length === 0) throw new Error('No valid student data found');
+    console.log(`Number of students: ${lines.length}`);
+    const fields = {};
+    lines.forEach((line) => {
+      const [firstname, , , field] = line.split(',');
+      if (field) {
+        if (!fields[field]) {
+	  fields[field] = [];
+	}
+	fields[field].push(firstname);
+      }
+    });
+    for (const [field, students] of Object.entries(fields)) {
+      console.log(`Number of students in ${field}: ${students.length}. List: ${students.join(', ')}`);
+    }
+  } catch (error) {
+      throw new Error('Cannot load the database');
+  }
+}
+
+module.exports = countStudents;
