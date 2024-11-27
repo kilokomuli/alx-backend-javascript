76f9f6b6bb0bdc0fcdb3ec2e74e55ebc87ee65b9
Emma <kilokoemmma175@gmail.com>
2024-11-27 14:09:15 +0000

    del directory

--- 0x05-Node_JS_basic/0-console.js
+++
@@ -1,7 +0,0 @@
-/**
- *  create a function named displayMessage
- *  Prints in STDOUT the string argument
- */
-const displayMessage = (message) => console.log(message);
-
-module.exports = displayMessage;
