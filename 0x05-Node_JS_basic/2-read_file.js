d5de92662d92a8dcb0472e10846a6cdfb2df97cf
Emma <kilokoemmma175@gmail.com>
2024-11-27 15:47:36 +0000

    fix lint

--- 0x05-Node_JS_basic/2-read_file.js
+++ 0x05-Node_JS_basic/2-read_file.js
@@ -1,4 +1,4 @@
-const fs = require('fs')
+const fs = require('fs');
 
 function countStudents(path) {
   try {
@@ -11,16 +11,16 @@ function countStudents(path) {
       const [firstname, , , field] = line.split(',');
       if (field) {
         if (!fields[field]) {
-	  fields[field] = [];
-	}
-	fields[field].push(firstname);
+          fields[field] = [];
+        }
+        fields[field].push(firstname);
       }
     });
     for (const [field, students] of Object.entries(fields)) {
       console.log(`Number of students in ${field}: ${students.length}. List: ${students.join(', ')}`);
     }
   } catch (error) {
-      throw new Error('Cannot load the database');
+    throw new Error('Cannot load the database');
   }
 }
 
---
+++ 0x05-Node_JS_basic/3-read_file_async.js
@@ -0,0 +1,38 @@
+const fs = require('fs');
+
+function countStudents(path) {
+  return new Promise((resolve, reject) => {
+    fs.readfile(path, 'utf8', (err, data) => {
+      if (err) {
+        reject(new Error('Cannot load the database'));
+	return;
+      }
+
+      const lines = data.split('\n').filter((line) => line.trim() !== '' && !line.startsWith('firstname'));
+      if (lines.length === 0) {
+        console.log('Number of students: 0');
+	resolve();
+	return;
+      }
+
+      console.log(`Number of students: ${lines.length}`);
+
+      const fields = {};
+      lines.forEach((line) => {
+        const [firstname, , , field] = line.split(',');
+        if (field) {
+	  if (!fields[field]) {
+	    fields[field] = [];
+	  }
+	  fields[field].push(firstname);
+	}
+      });
+      for (const [field, students] of Object.entries(fields)) {
+        console.log(`Number of students in ${field}: ${students.length}. List: ${students.join(', ')}`);
+      }
+      resolve();
+    });
+  });
+}
+
+module.exports = countStudents;
