fd21424273185888e29ec39b3e3ea0a6baa7b25e
Emma <kilokoemmma175@gmail.com>
2024-11-27 16:28:35 +0000

    fix lint

--- 0x05-Node_JS_basic/3-read_file_async.js
+++ 0x05-Node_JS_basic/3-read_file_async.js
@@ -5,14 +5,14 @@ function countStudents(path) {
     fs.readFile(path, 'utf8', (err, data) => {
       if (err) {
         reject(new Error('Cannot load the database'));
-	return;
+        return;
       }
 
       const lines = data.split('\n').filter((line) => line.trim() !== '' && !line.startsWith('firstname'));
       if (lines.length === 0) {
         console.log('Number of students: 0');
-	resolve();
-	return;
+        resolve();
+        return;
       }
 
       console.log(`Number of students: ${lines.length}`);
@@ -21,11 +21,11 @@ function countStudents(path) {
       lines.forEach((line) => {
         const [firstname, , , field] = line.split(',');
         if (field) {
-	  if (!fields[field]) {
-	    fields[field] = [];
-	  }
-	  fields[field].push(firstname);
-	}
+          if (!fields[field]) {
+            fields[field] = [];
+          }
+          fields[field].push(firstname);
+        }
       });
       for (const [field, students] of Object.entries(fields)) {
         console.log(`Number of students in ${field}: ${students.length}. List: ${students.join(', ')}`);
